dd3e9a4f65b48dedded0b73cbe1104fa963d5054
nightly build <dev.tobeh@gmail.com>
2025-01-21 18:35:17 +0000

    toobeeh/skribbltypo@f65f240 add missing api dto

--- chrome/assets/content.ts-loader-T5D605YO.js
+++ chrome/assets/content.ts-loader-T5D605YO.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-DLLwf8Y2.js")
+      chrome.runtime.getURL("assets/chunk-Brzv26I3.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
