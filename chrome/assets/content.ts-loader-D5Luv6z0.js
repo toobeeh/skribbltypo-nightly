569cbfa6193045474d089240ef161ec7100c36e6
nightly build <dev.tobeh@gmail.com>
2025-01-22 19:05:27 +0000

    fix drawing glitches when cursor out of canvas bounds toobeeh/skribbltypo#173 original commit: toobeeh/skribbltypo@c8b174b

--- chrome/assets/content.ts-loader-D5Luv6z0.js
+++ chrome/assets/content.ts-loader-D5Luv6z0.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-ZS10wNcc.js")
+      chrome.runtime.getURL("assets/chunk-T_Tp3Uam.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
