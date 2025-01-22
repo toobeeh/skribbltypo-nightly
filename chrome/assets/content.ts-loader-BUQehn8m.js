6b24f060257692aa40cd44ded2f8577575353a50
nightly build <dev.tobeh@gmail.com>
2025-01-22 11:39:50 +0000

    toobeeh/skribbltypo@2a98ff1 fix toobeeh/skribbltypo#168 drawing over other's images

--- chrome/assets/content.ts-loader-BUQehn8m.js
+++ chrome/assets/content.ts-loader-BUQehn8m.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-Brzv26I3.js")
+      chrome.runtime.getURL("assets/chunk-DS5h2fHd.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
