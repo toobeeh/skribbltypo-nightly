343518bf94baac868fff28a5d49bab79bac086b9
nightly build <dev.tobeh@gmail.com>
2025-01-22 20:15:20 +0000

    fix zoom & line tool pointer event priorities toobeeh/skribbltypo#159 original commit: toobeeh/skribbltypo@1bfff8f

--- chrome/assets/content.ts-loader-D5B2JBls.js
+++ chrome/assets/content.ts-loader-D5B2JBls.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-Bwn8zImb.js")
+      chrome.runtime.getURL("assets/chunk-Dp16rmk6.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
