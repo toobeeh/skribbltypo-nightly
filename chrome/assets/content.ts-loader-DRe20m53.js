4c4b3f4a95290cfbfd85633c0a4701ece4087cf8
nightly build <dev.tobeh@gmail.com>
2025-01-22 19:22:18 +0000

    fix straight lines ignoring brush style toobeeh/skribbltypo#154 original commit: toobeeh/skribbltypo@5040f4c

--- chrome/assets/content.ts-loader-DRe20m53.js
+++ chrome/assets/content.ts-loader-DRe20m53.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-T_Tp3Uam.js")
+      chrome.runtime.getURL("assets/chunk-Bwn8zImb.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
