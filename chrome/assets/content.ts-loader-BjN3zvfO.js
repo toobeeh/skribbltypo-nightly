6b1c6d014ac7a800746d9691fd6474efa4967bf0
nightly build <dev.tobeh@gmail.com>
2025-01-23 20:32:39 +0000

    add right-click to remove for scene/sprites in cabin toobeeh/skribbltypo#206 toobeeh/skribbltypo#208 original commit: toobeeh/skribbltypo@f63fa90

--- chrome/assets/content.ts-loader-BjN3zvfO.js
+++ chrome/assets/content.ts-loader-BjN3zvfO.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-CuzYVR8N.js")
+      chrome.runtime.getURL("assets/chunk-q1y6rEma.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
