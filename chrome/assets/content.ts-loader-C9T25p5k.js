2c1ad9dc30679003aa9405c1ef337e27e243db3f
nightly build <dev.tobeh@gmail.com>
2025-01-23 13:09:20 +0000

    fix stuck tooltips when cursor moves out of document body toobeeh/skribbltypo#170 original commit: toobeeh/skribbltypo@6c8b34d

--- chrome/assets/content.ts-loader-C9T25p5k.js
+++ chrome/assets/content.ts-loader-C9T25p5k.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-CI8Ly1Sg.js")
+      chrome.runtime.getURL("assets/chunk-CZBvFXdZ.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
