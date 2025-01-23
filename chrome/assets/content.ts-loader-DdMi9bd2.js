6bae8a1f4fb499f800abfade237bcc5aff8857b1
nightly build <dev.tobeh@gmail.com>
2025-01-23 20:14:01 +0000

    fix rainbow sprites ingame toobeeh/skribbltypo#215 original commit: toobeeh/skribbltypo@f428dd4

--- chrome/assets/content.ts-loader-DdMi9bd2.js
+++ chrome/assets/content.ts-loader-DdMi9bd2.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-B5b64tsd.js")
+      chrome.runtime.getURL("assets/chunk-CuzYVR8N.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
