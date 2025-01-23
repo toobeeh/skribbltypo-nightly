77a621cd11cb8183225fe42039978a6f1541fae0
nightly build <dev.tobeh@gmail.com>
2025-01-23 13:05:26 +0000

    clear hotkeys when recording, auto-enable when saved toobeeh/skribbltypo#188 original commit: toobeeh/skribbltypo@11686ac

--- chrome/assets/content.ts-loader-BEOfGs7F.js
+++ chrome/assets/content.ts-loader-BEOfGs7F.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-BOreRnMO.js")
+      chrome.runtime.getURL("assets/chunk-CI8Ly1Sg.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
