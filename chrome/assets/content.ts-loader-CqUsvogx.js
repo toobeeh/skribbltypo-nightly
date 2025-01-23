0a1b9a79380a48e2ae8bbae113e041df26a899c2
nightly build <dev.tobeh@gmail.com>
2025-01-23 13:14:49 +0000

    fix chat controls blocking chat copying at the very top toobeeh/skribbltypo#179 original commit: toobeeh/skribbltypo@14a1e9c

--- chrome/assets/content.ts-loader-CqUsvogx.js
+++ chrome/assets/content.ts-loader-CqUsvogx.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-CZBvFXdZ.js")
+      chrome.runtime.getURL("assets/chunk-iIbDVSJD.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
