d3a9319e93f7095256920a415bd7c6900675c564
nightly build <dev.tobeh@gmail.com>
2025-01-23 18:29:47 +0000

    fix default player to kick and improve error messages toobeeh/skribbltypo#182 original commit: toobeeh/skribbltypo@fcf42c2

--- chrome/assets/content.ts-loader-BPvnM6U1.js
+++ chrome/assets/content.ts-loader-BPvnM6U1.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-CpTYNnDA.js")
+      chrome.runtime.getURL("assets/chunk-B5b64tsd.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
