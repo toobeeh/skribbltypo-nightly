9f86ac272a2d2c1de77c75a2612e61060db541c4
nightly build <dev.tobeh@gmail.com>
2025-01-24 02:58:08 +0000

    wait for socket.io on load and debug interceptor toobeeh/skribbltypo#185 original commit: toobeeh/skribbltypo@e0012c5

--- chrome/assets/content.ts-loader-C5du8vf8.js
+++ chrome/assets/content.ts-loader-C5du8vf8.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-DtIFTmin.js")
+      chrome.runtime.getURL("assets/chunk-xQ82n4tI.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
