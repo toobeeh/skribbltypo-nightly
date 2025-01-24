bf663da3508d9bc8091944347c0fd92e97a83dce
nightly build <dev.tobeh@gmail.com>
2025-01-24 23:55:56 +0000

    Merge pull request #241 from foley-dev/bugfix/recall-initial-value
    fix initial value in chat recall
    original commit: toobeeh/skribbltypo@9609466

--- chrome/assets/content.ts-loader-DUk3wYjI.js
+++ chrome/assets/content.ts-loader-DUk3wYjI.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-CVCOsKs8.js")
+      chrome.runtime.getURL("assets/chunk-JfZ_EE-y.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
