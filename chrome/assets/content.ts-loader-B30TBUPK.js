adb3f8ef911864ff105cbbec6a411557f03fbe75
nightly build <dev.tobeh@gmail.com>
2025-01-24 13:42:18 +0000

    use floor instead ceil toobeeh/skribbltypo#231 original commit: toobeeh/skribbltypo@8c67bd8

--- chrome/assets/content.ts-loader-B30TBUPK.js
+++ chrome/assets/content.ts-loader-B30TBUPK.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-46fb34Lz.js")
+      chrome.runtime.getURL("assets/chunk-DISXiEZN.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
