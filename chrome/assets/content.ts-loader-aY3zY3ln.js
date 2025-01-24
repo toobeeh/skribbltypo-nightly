7a39e0c14cd60592e1894f838b28503272295d9c
nightly build <dev.tobeh@gmail.com>
2025-01-24 00:34:59 +0000

    show hint when no members online original commit: toobeeh/skribbltypo@ba105e4

--- chrome/assets/content.ts-loader-aY3zY3ln.js
+++ chrome/assets/content.ts-loader-aY3zY3ln.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-DIAsy5Va.js")
+      chrome.runtime.getURL("assets/chunk-C8yxsPiZ.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
