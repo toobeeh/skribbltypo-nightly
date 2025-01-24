1292b8ce8c484e2fb58f1458bb9d13f76bb68c14
nightly build <dev.tobeh@gmail.com>
2025-01-24 00:03:01 +0000

    populate lobby tooltips, fix lobby button join toobeeh/skribbltypo#206 toobeeh/skribbltypo#193 original commit: toobeeh/skribbltypo@cfdf015

--- chrome/assets/content.ts-loader-BbqRVHgM.js
+++ chrome/assets/content.ts-loader-BbqRVHgM.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-q1y6rEma.js")
+      chrome.runtime.getURL("assets/chunk-DIAsy5Va.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
