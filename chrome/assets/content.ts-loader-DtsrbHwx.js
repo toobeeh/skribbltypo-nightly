c4dc769c6be11c1de7306c090f358f89404e6117
nightly build <dev.tobeh@gmail.com>
2025-01-25 02:39:14 +0000

    add paste instant toggle original commit: toobeeh/skribbltypo@a2f7462

--- chrome/assets/content.ts-loader-DtsrbHwx.js
+++ chrome/assets/content.ts-loader-DtsrbHwx.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/chunk-JfZ_EE-y.js")
+      chrome.runtime.getURL("assets/chunk-DzuygB0e.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
