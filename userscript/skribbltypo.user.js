8379aa56a8baa7552e8d754ee9cbb089578ea0fb
nightly build <dev.tobeh@gmail.com>
2025-03-31 19:22:18 +0000

    fix release zip output structure original commit: toobeeh/skribbltypo@813763e

--- userscript/skribbltypo.user.js
+++ userscript/skribbltypo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         skribbltypo
 // @namespace    vite-plugin-monkey
-// @version      27.0.0 beta-usc 6d29a68
+// @version      27.0.0 beta-usc 813763e
 // @author       tobeh
 // @description  The toolbox for everything you need on skribbl.io
 // @match        https://skribbl.io/*
@@ -439,7 +439,7 @@
       return isIteratorProp(target, prop) || oldTraps.has(target, prop);
     }
   }));
-  const pageReleaseDetails = { version: "27.0.0", versionName: "27.0.0 beta-usc 6d29a68", runtime: "userscript" };
+  const pageReleaseDetails = { version: "27.0.0", versionName: "27.0.0 beta-usc 813763e", runtime: "userscript" };
   const gamePatch = `((h, c, d, O) => {
   let P = 28,
     Y = 57,
