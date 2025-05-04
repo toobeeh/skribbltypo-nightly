7a670a9a24f03e3751c37ed0ba7807a36816ac5e
nightly build <dev.tobeh@gmail.com>
2025-05-04 22:46:41 +0000

    Merge branch 'main' into develop original commit: toobeeh/skribbltypo@f73dd15

--- userscript/skribbltypo.user.js
+++ userscript/skribbltypo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         skribbltypo
 // @namespace    vite-plugin-monkey
-// @version      27.1.0 beta-usc e9ce7d5
+// @version      27.1.0 beta-usc f73dd15
 // @author       tobeh
 // @description  The toolbox for everything you need on skribbl.io
 // @updateURL    https://get.typo.rip/userscript/skribbltypo.user.js
@@ -446,7 +446,7 @@
       return isIteratorProp(target, prop) || oldTraps.has(target, prop);
     }
   }));
-  const pageReleaseDetails = { version: "27.1.0", versionName: "27.1.0 beta-usc e9ce7d5", runtime: "userscript" };
+  const pageReleaseDetails = { version: "27.1.0", versionName: "27.1.0 beta-usc f73dd15", runtime: "userscript" };
   const gamePatch = `((h, c, d, O) => {
   let P = 28,
     Y = 57,
