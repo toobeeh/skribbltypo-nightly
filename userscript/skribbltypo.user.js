5ad15e9e91d6076b13835689cedf6b597dc4637b
nightly build <dev.tobeh@gmail.com>
2025-10-12 23:35:11 +0000

    increment version original commit: toobeeh/skribbltypo@b87f04d

--- userscript/skribbltypo.user.js
+++ userscript/skribbltypo.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         skribbltypo
 // @namespace    vite-plugin-monkey
-// @version      27.1.3 beta-usc 0eb0c08
+// @version      28.0.1 beta-usc b87f04d
 // @author       tobeh
 // @description  The toolbox for everything you need on skribbl.io
 // @updateURL    https://get.typo.rip/userscript/skribbltypo.user.js
@@ -446,7 +446,7 @@
       return isIteratorProp(target, prop) || oldTraps.has(target, prop);
     }
   }));
-  const pageReleaseDetails = { version: "27.1.3", versionName: "27.1.3 beta-usc 0eb0c08", runtime: "userscript" };
+  const pageReleaseDetails = { version: "28.0.1", versionName: "28.0.1 beta-usc b87f04d", runtime: "userscript" };
   const gamePatch = `((h, c, d, O) => {
   let P = 28,
     Y = 57,
